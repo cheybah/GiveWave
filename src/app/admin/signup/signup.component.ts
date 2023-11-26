d9a46c7d89ac83485e2d0d168f12f1ac1e0eecdf
cheybah <cheymabahroun@gmail.com>
2023-11-26 12:42:20 +0100

    fixed input control for auth forms

--- src/app/admin/signup/signup.component.ts
+++ src/app/admin/signup/signup.component.ts
@@ -10,6 +10,8 @@ import {AuthService} from "../../services/auth.service";
 })
 export class SignupComponent {
 
+  errorMessage: string = ''; // Add this line
+
   user: User = {
     id: Math.floor(1000 + Math.random() * 9000),
     name: '',
@@ -25,11 +27,33 @@ export class SignupComponent {
   ) { }
 
   signUp(): void {
-    this.authService.signUp(this.user).subscribe(user => {
-      console.log('User created', user.id);
-      this.router.navigate(['/login']);
-    });
-
+    if (this.validateForm()) {
+      this.authService.signUp(this.user).subscribe(
+        (user) => {
+          console.log('User created', user.id);
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.log('Sign up failed', error);
+          this.errorMessage = 'Sign up failed. Please check the required fields.';
+        }
+      );
+    } else {
+      this.errorMessage = 'Please fill out all required fields correctly.';
+    }
   }
+
+  validateForm(): boolean {
+    if (!this.user.name || !this.user.lastname || !this.user.email || !this.user.password || !this.user.bank_info) {
+      return false;
+    }
+    return true;
+  }
+
+  togglePasswordVisibility(): void {
+    const passwordInput = document.querySelector('[name="password"]') as HTMLInputElement;
+    passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+  }
+  
 
 }
