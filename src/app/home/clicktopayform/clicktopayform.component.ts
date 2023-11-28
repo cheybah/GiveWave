cd916e295d9747700c224ebc0aed5dd9e02273b0
Mehdii909 <benghazi_mehdi@yahoo.com>
2023-11-28 14:31:36 +0100

    commit click to pay fixed bug

--- src/app/home/clicktopayform/clicktopayform.component.ts
+++ src/app/home/clicktopayform/clicktopayform.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {PotService} from "../../services/pot.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Pot} from "../../models/Pot";
 import {AuthService} from "../../services/auth.service";
 import {User} from "../../models/User";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ContributorService} from "../../services/contributor.service";
 
 @Component({
   selector: 'app-clicktopayform',
@@ -35,19 +36,17 @@ export class ClicktopayformComponent implements OnInit{
     private route: ActivatedRoute,
     private potService: PotService,
     private authService: AuthService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private contributorService: ContributorService,
+    private router: Router
   ) {
     this.paymentForm = this.formBuilder.group({
-      firstName: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)],
-      lastName: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)],
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
       email: ['', [Validators.required, Validators.pattern(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
-      address: [''],
-      country: [''],
-      state: [''],
-      zip: [''],
       saveInfo: [false],
       paymentMethod: ['credit', Validators.required],
-      ccName: ['', Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)],
+      ccName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/)]],
       ccNumber: ['', [Validators.required, Validators.pattern(/^\d{16}$/)]], // 16 digits for credit card number
       ccExpiration: ['', [Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)]], // MM/YY format
       ccCvv: ['', [Validators.required, Validators.pattern(/^\d{3}$/)]], // 3 digits for CVV
@@ -69,6 +68,29 @@ export class ClicktopayformComponent implements OnInit{
   }
 
   submitForm() {
-    console.log(this.paymentForm.value);
+
+    const contributorData = {
+      name: this.paymentForm.value.firstName + ' ' + this.paymentForm.value.lastName,
+      email: this.paymentForm.value.email,
+      bank_info: this.paymentForm.value.ccNumber, // Replace with actual bank_info logic
+      donation_amount: this.contributionAmount,
+      pot_id: this.potId
+    };
+
+    this.contributorService.addContributor(contributorData).subscribe(
+      (newContributor: any) =>
+      {
+        this.pot.current_amount +=  this.contributionAmount;
+        this.pot.contributors.push(newContributor.id);
+        this.potService.updatePot(this.pot).subscribe(
+          () => {
+            // Pot updated successfully
+            // Navigate back to home page
+            this.router.navigate(['/home']);
+          }
+        )
+      });
+
+
   }
 }
